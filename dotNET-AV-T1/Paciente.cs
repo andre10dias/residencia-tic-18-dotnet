4d2a3f84b6af1ce30cab66e6ba6401905b55658a
andre10dias <andre10dias@ymail.com>
2023-11-29 11:37:00 -0300

    Validando cpf

--- dotNET-AV-T1/Paciente.cs
+++ dotNET-AV-T1/Paciente.cs
@@ -4,17 +4,38 @@ namespace dotNET_AV_T1 {
 
         public DateTime DataNascimento { get; set; }
 
-        public string Cpf { get; set; }
+        public string Cpf 
+        { 
+            get {return Cpf;}
+            set
+            {
+                if (string.IsNullOrEmpty(value.Trim())) 
+                {
+                    throw new ArgumentNullException("O campo CPF é obrigatório.");
+                }
+
+                if (!Cpf.All(char.IsDigit))
+                {
+                    throw new ArgumentException("O campo CPF precisa ter apenas números.");
+                }
+
+                if (Cpf.Length != 11)
+                {
+                    throw new ArgumentException("O campo CPF precisa ter 11 caracteres.");
+                }
+            }
+        }
 
         public string Sexo { get; set; }
 
         public string Sintomas { get; set; }
 
-        public Paciente(string nome, DateTime dataNascimento, string cpf, string sintomas)
+        public Paciente(string nome, DateTime dataNascimento, string cpf, string sexo, string sintomas)
         {
             Nome = nome;
             DataNascimento = dataNascimento;
             Cpf = cpf;
+            Sexo = sexo;
             Sintomas = sintomas;
         }
     }
